b1a72bc38875af57bf639b3d93b42741bd4fcbb7
sonali <sona120920@gmail.com>
2021-01-15 22:49:23 +0530

    Home page updated

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
     // Transition effect for navbar 
     $(window).scroll(function() {
-        // checks if window is scrolled more than 500px, adds/removes solid class
-        if ($(this).scrollTop() > 500) {
+        // checks if window is scrolled more than 5px, adds/removes solid class
+        if ($(this).scrollTop() > 5) {
             $('.navbar').addClass('solid');
         } else {
             $('.navbar').removeClass('solid');
         }
     });
 });
+
