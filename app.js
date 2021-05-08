ac133750b7a24568c8a89fb5959dd7c12cff17a8
simsam0502 <simransaigal@icloud.com>
2021-05-08 21:08:19 +0530

    Routing added

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const http = require('http');
-
+const fs = require('fs');
+const path = require('path');
 const port = 3000;
 
-const server = http.createServer((request, respond) => {
-  respond.statusCode = 200;
-  respond.setHeader('Content-Type', 'text/plain');
-  respond.end('Hello!');
+const server = http.createServer((req, res) => {
+  const filePath = req.url=='/'||req.url=='/favicon.ico'?'/views/index.html':req.url;
+  const extension = path.extname(filePath).slice(1);
+  const type = extension=='png'||extension=='jpg'||extension=='jpeg'?'image':'text';
+  res.setHeader('Content-Type', type+'/'+extension);
+  fs.readFile('.'+filePath,(err,data)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.write(data);
+    } 
+    res.end();
+  });
 });
 
 server.listen(port, () => {
